ebf674370445a213fc0bdf39ea36fbac1760a279
T. Stephen Raj <twinstephen001@gmail.com>
2026-02-18 09:56:08 +0530

    test: added AI module test script

---
+++ ai-engine/test.js
@@ -0,0 +1,13 @@
+import { systemPrompt } from "./systemPrompt.js";
+import { validateQuery } from "./queryValidator.js";
+
+const testQuery = "Show all users from Chennai";
+
+console.log("System Prompt Loaded");
+
+const fakeAIOutput = 'db.users.find({ city: "Chennai" })';
+
+const result = validateQuery(fakeAIOutput);
+
+console.log("Generated Query:", fakeAIOutput);
+console.log("Validation:", result);
